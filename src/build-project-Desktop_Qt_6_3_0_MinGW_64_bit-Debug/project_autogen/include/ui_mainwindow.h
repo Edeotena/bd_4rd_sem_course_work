ffbb251e64e40a17067bb46f42cdd9c4bc973c2b
edeotena <edeotena13@gmail.com>
2022-05-03 02:29:34 +0300

    some SQL (and added path to psql's bin into Qt's PATH)

--- src/build-project-Desktop_Qt_6_3_0_MinGW_64_bit-Debug/project_autogen/include/ui_mainwindow.h
+++ src/build-project-Desktop_Qt_6_3_0_MinGW_64_bit-Debug/project_autogen/include/ui_mainwindow.h
@@ -11,9 +11,13 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +26,9 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QTextBrowser *textBrowser;
+    QTableView *tableView;
+    QPushButton *pushButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +36,22 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(994, 453);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setGeometry(QRect(90, 110, 256, 192));
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+        tableView->setGeometry(QRect(640, 120, 256, 192));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(450, 40, 83, 29));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 994, 25));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +65,7 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
     } // retranslateUi
 
 };
--- src/project/mainwindow.cpp
+++ src/project/mainwindow.cpp
@@ -13,3 +13,39 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+QSqlDatabase set_database() {
+    QSqlDatabase database = QSqlDatabase::addDatabase("QPSQL");
+    database.setHostName("localhost");
+    database.setPort(5432);
+    database.setDatabaseName("db");
+    database.setUserName("postgres");
+    database.setPassword("?");
+    return database;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QSqlDatabase db = set_database();
+        if (db.open()) {
+            ui->textBrowser->append("DB successfully opened!\n");
+            for (const auto& i : db.tables()) {
+                ui->textBrowser->append(i);
+            }
+        } else {
+            ui->textBrowser->append("все плохо");
+        }
+
+        table = new QSqlTableModel(this, db);
+        table->setTable("territory_data");
+        table->select();
+        ui->tableView->setModel(table);
+        ui->tableView->resizeColumnsToContents();
+        ui->tableView->resizeRowsToContents();
+        //ui->tableView->resize(QSize(ui->tableView->horizontalHeader()->geometry()))
+
+        db.close();
+        ui->textBrowser->append("\nDB successfully closed!");
+
+}
+
