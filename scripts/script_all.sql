b187bdc2502633da54446031bd56eb980e51fb70
edeotena <edeotena13@gmail.com>
2022-05-10 17:08:59 +0300

    script all

---
+++ scripts/script_all.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE db;
+\c db
+\i 'C:/bd_4rd_sem_course_work/scripts/script_create_bd.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_station.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_worker.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_carriage.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_locomotive.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_carriage_locomotive.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_status.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_company.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_order.sql'
+\i 'C:/bd_4rd_sem_course_work/scripts/script_fill_brigade.sql'
+	
